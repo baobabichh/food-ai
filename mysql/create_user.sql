a697eb8f311724086d094b689bd819baf52c57a2
goshan9 <pickup2legs@gmail.com>
2025-03-07 23:06:51 +0100

    changes

--- mysql/create_user.sql
+++ mysql/create_user.sql
@@ -3,10 +3,10 @@ use food_ai;
 
 USE mysql;
 
-CREATE USER 'user'@'%' IDENTIFIED BY 'pass';
+CREATE USER 'app_user'@'%' IDENTIFIED BY '';
 
 -- Grant all privileges on the specific database to the user
-GRANT ALL PRIVILEGES ON food_ai.* TO 'user'@'%';
+GRANT ALL PRIVILEGES ON food_ai.* TO 'app_user'@'%';
 
 -- Apply changes
 FLUSH PRIVILEGES;
