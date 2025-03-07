314c1365b5e19082bd445212cfe406e7260b88ce
goshan9 <pickup2legs@gmail.com>
2025-03-07 23:20:19 +0100

    changes

--- mysql/FoodRecognitionRequest.sql
+++ mysql/FoodRecognitionRequest.sql
@@ -3,6 +3,6 @@ CREATE TABLE FoodRecognitionRequests
     ID BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     ImgBase64 LONGTEXT,
     ImgType VARCHAR(20),
-    Status TINYINT,
+    Status TINYINT DEFAULT 0, -- 1 pending, 2 processing, 3 error, 4 success
     CreateTS TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
